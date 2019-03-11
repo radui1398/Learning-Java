ba0f9c91cea56e88eb896a90c980ba3010867d5b
radui1398 <radui1398@gmail.com>
2019-03-12 01:22:44 +0200

    Am adaugat si comentarii

--- lab4/src/CatalogIO.java
+++ lab4/src/CatalogIO.java
@@ -1,5 +1,8 @@
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 
+/**
+ * Nu am aflat inca ce rost are
+ */
 public class CatalogIO {
 }
--- lab4/src/Main.java
+++ lab4/src/Main.java
@@ -1,7 +1,13 @@
 import java.time.LocalDate;
 import java.time.Month;
 
+/**
+ * Clasa principala Main
+ */
 public class Main {
+    /**
+     * Inima aplicatiei :)
+     */
     public static void main(String[] args) {
         Catalog catalog = new Catalog("/home/radu/graphs");
         catalog.add (new Graph("K4", "complete/k4.tgf", "complete/view/k4.png"));
